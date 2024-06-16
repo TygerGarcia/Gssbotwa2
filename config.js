85c583df5bd89dd3f1252567a0f0d9ae60b21679
TygerGarcia <thabetheesihle6@gmail.com>
2024-06-16 09:33:03 +0200

    Update config.js

--- config.js
+++ config.js
@@ -7,13 +7,13 @@ global.link = 'https://whatsapp.com/channel/0029VaWJMi3GehEE9e1YsI1S'
 global.linkGroup = 'https://chat.whatsapp.com/E3PWxdvLc7ZCp1ExOCkEGp'
 
 global.apikey = 'bf2d2cf29b3edc604b447983';
-global.ownername = "𝑆𝛪𝐷 𝐵𝛨𝛥𝛪";
-global.owner = ['917050906659'];
-global.premium = ['917050906659'];
+global.ownername = "MAMASBOY";
+global.owner = ['27787896221'];
+global.premium = ['27787896221'];
 global.packname = "𝚜𝚝𝚒𝚌𝚔𝚎𝚛 𝚋𝚢";
 global.author = "𝐆𝐒𝐒 𝚩𝚯𝚻𝐖𝚫";
 global.sessionName = 'session';
-global.prefa = [".", "!", "/", "?"];
+global.prefa = ["."];
 global.typemenu = "v1"
 global.menuType = "2"
 global.autoswview = true
@@ -24,7 +24,7 @@ global.autoRecord = false
 global.autoreact = false
 global.onlygroup = false
 global.onlypc = false
-global.welcome = true
+global.welcome = false
 global.autoBlock = true
  
 /*REPLY MESSAGE*/
